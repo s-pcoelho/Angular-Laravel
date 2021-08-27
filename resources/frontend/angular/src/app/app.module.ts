f1a592071189011bb608c7141b8332ca1772d96d
Sophia Coelho <sophia.coelho@etec.sp.gov.br>
2021-08-27 15:07:14 -0300

    inserting toolbar

--- resources/frontend/angular/src/app/app.module.ts
+++ resources/frontend/angular/src/app/app.module.ts
@@ -4,15 +4,44 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './views/home/home.component';
+
+//Angular Material
+import {MatCardModule } from '@angular/material/card';
+import {MatDividerModule } from '@angular/material/divider';
+import {MatChipsModule } from '@angular/material/chips';
+import {MatToolbarModule } from '@angular/material/toolbar';
+import {MatIconModule } from '@angular/material/icon';
+import {MatTabsModule } from '@angular/material/tabs';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatInputModule} from '@angular/material/input';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatDividerModule,
+    MatChipsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatTabsModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatProgressBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
