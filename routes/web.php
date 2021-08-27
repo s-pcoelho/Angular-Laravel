93bf0a3dde5b0d30191ee4e8f653d593313e8c57
Sophia Coelho <sophia.coelho@etec.sp.gov.br>
2021-08-27 12:33:50 -0300

    create controller and edit web route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AngularController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,3 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route :: any ('/ {any}', [AngularController :: class, 'index']) -> where ('any', '^ (?! api). * $');
